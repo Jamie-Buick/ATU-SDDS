bf9efb94dcc1c811ba5a04472079fe8f0051b44e
Jamie Buick <jamiebuick28@gmail.com>
2025-10-01 21:53:16 +0100

    Tree data structure example

---
+++ Tree/src/ie/gmit/dip/Node.java
@@ -0,0 +1,23 @@
+package ie.gmit.dip;
+
+import java.util.*;
+
+public interface Node<T> { // T can be any data type, its a generic. Gives us flexibility
+
+	public T getData();
+	public void setData(T data);
+	
+	public boolean isRoot();
+	public boolean isLeaf();
+	public Node<T> getParent();
+	public List<Node<T>> children();
+	
+	public void addChild(Node<T> child);
+	public void removeChild(Node<T> child);
+	public void hasChild(Node<T> child);
+	public boolean hasSibling();
+	public List<Node<T>> siblings();
+	
+	
+	
+}
